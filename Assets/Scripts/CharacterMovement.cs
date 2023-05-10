16daf76666c6ef395ef7a49097cf781c5e1fcf9a
Etholacto <cedric@kornacki.net>
2023-05-10 20:19:25 +0200

    Changes to CharacterMovement and Inventory System. Add test collision for adding amount.

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,12 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterMovement : MonoBehaviour
 {
+    public float Speed = 5.0f;
+
+    public float drag;
+
     Rigidbody rb;
-    [SerializeField] float movementSpeed = 5.0f;
-    [SerializeField] float jumpForce = 5.0f;
+
+    float horizontalInput;
+    float verticalInput;
+
+    private Vector3 _moveDir = Vector3.zero;
+
+    float WoodAmount = 0;
+    float StoneAmount = 0;
+
+    public TMPro.TMP_Text WoodAmountText;
+    public TMPro.TMP_Text StoneAmountText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,14 +31,52 @@ public class CharacterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        //Get Input
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
 
-        rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y , verticalInput * movementSpeed);
+        SpeedControl();
 
-        if (Input.GetButtonDown("Jump"))
+        ChangeResourceText();
+
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        //calculate movement direction
+        _moveDir = transform.forward * verticalInput + transform.right * horizontalInput;
+
+        rb.AddForce(_moveDir.normalized * Speed * 10f, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        //limit velocity if over Speed
+        if (flatVel.magnitude > Speed)
         {
-            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+            Vector3 limitedVel = flatVel.normalized * Speed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+
+    private void ChangeResourceText()
+    {
+        WoodAmountText.text = string.Format("{0}", WoodAmount);
+        StoneAmountText.text = string.Format("{0}", StoneAmount);
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "WoodObj")
+        {
+            WoodAmount += 1;
         }
     }
 }
